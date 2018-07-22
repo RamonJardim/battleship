ea9821f30b6f38841ba5f07a648f67d58f736f30
Ramon <ramon-neres@hotmail.com>
2018-07-21 22:19:40 -0300

    Reorganizando

---
+++ graphics/Main.java
@@ -0,0 +1,48 @@
+package graphics;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.awt.Rectangle;
+
+import elements.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import ships.*;
+
+public class Main extends Application{
+    
+    Button[][] table = new Button[10][10];
+    
+    public static void main(String[] args) {
+
+        launch(args);
+        
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Battleship");
+        GridPane gridPane = new GridPane();
+        
+        for (int j = 0; j < table.length; j++) {
+            for (int i = 0; i < table[0].length; i++) {
+                table[i][j] = new Button (j +","+i);
+                table[i][j].setPrefSize(35, 35);
+                GridPane.setConstraints(table[i][j], i, j);
+                gridPane.getChildren().add(table[i][j]);
+            }
+        }
+        
+        Scene scene = new Scene(gridPane, 750,350);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+
+}
